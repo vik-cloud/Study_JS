093eb202cf6549ee43f0b0be7b3a831e551cc4a0
Makarenko Viktor <nevermind.13@mail.ru>
2020-10-14 17:19:51 +0400

    Исправлен урок N4 - обязательное. Переделано присвоение функций переменным.

--- script/script.js
+++ script/script.js
@@ -1,25 +1,44 @@
 'use strict';
-let amount1 = +prompt('Во сколько это обойдется?');
-let amount2 = +prompt('Во сколько это обойдется?');
-let expensesMonth = getExpensesMonth();
+
 let money = +prompt('Ваш месячный доход?', 50000);
-let accumulatedMonth = getAccumulatedMonth();
-let mission = 500000;
-let targetMonth = getTargetMonth();
 let income = 'фриланс';
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
 let deposit = confirm('Есть ли у вас депозит в банке?');
+let mission = 500000;
 let period = 10;
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
 
 
+function showTypeOf(data) {
+  console.log('Тип данных значения "' + data + '":', typeof data);
+} 
 
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(addExpenses);
 
 console.log(addExpenses.toLowerCase().split(', '));
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
+
+
+function getExpensesMonth() {
+  return amount1 + amount2;
+}; 
+let expensesMonth = getExpensesMonth();
+
+function getAccumulatedMonth() {
+ return money - expensesMonth;
+}; 
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth() {
+  return  Math.ceil(mission / accumulatedMonth);
+}; 
+let targetMonth = getTargetMonth();
+
 
 if (accumulatedMonth < 0){
   console.log('Цель недостижима, Вы в убытке!');
@@ -47,23 +66,7 @@ function getStatusIncome() {
   }
 }
 
-function getExpensesMonth() {
-  return amount1 + amount2;
-}; 
-
 console.log('Расходы за месяц:', expensesMonth, 'руб.');
-
-function getAccumulatedMonth() {
- return money - expensesMonth;
-}; 
-
-function getTargetMonth() {
-  return  Math.ceil(mission / accumulatedMonth);
-}; 
-
-function showTypeOf(data) {
-  console.log('Тип данных значения "' + data + '":', typeof data);
-} 
 
 getStatusIncome();
 
