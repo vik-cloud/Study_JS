935c5c0f18059157cc8e9622dcbb61e9ec83a5f6
Makarenko Viktor <nevermind.13@mail.ru>
2020-10-25 13:43:57 +0400

    Lesson-10: complited 5 steps

--- script/script.js
+++ script/script.js
@@ -4,10 +4,12 @@ const bookParent = document.querySelector('.books'),
       bookAll = document.querySelectorAll('.book'),
       body = document.querySelector('body'),
       bookLinkAll = document.querySelectorAll('.book a'),
-      advertising = document.querySelector('.adv');
+      advertising = document.querySelector('.adv'),
+      bookListAll = document.querySelectorAll('ul'),
+      bookListItemAll = document.querySelectorAll('li');
       
 
-// console.log(bookLinkAll);
+console.log(bookListItemAll);
 
 bookParent.prepend(bookAll[1]);
 bookParent.append(bookAll[2]);
@@ -18,6 +20,14 @@ body.setAttribute('style','background-image: url(./image/you-dont-know-js.jpg)')
 bookLinkAll[4].textContent = 'Книга 3. this и Прототипы Объектов';
 
 advertising.remove();
+
+bookListItemAll[3].after(bookListItemAll[6]);
+bookListItemAll[6].after(bookListItemAll[8]);
+bookListItemAll[9].after(bookListItemAll[2]);
+
+bookListItemAll[50].after(bookListItemAll[48]);
+bookListItemAll[47].after(bookListItemAll[55]);
+bookListItemAll[53].after(bookListItemAll[51]);
 
 
 
