22d8204c96a3ca395d5a73e441c6f9748ee945a8
Makarenko Viktor <nevermind.13@mail.ru>
2020-10-25 12:57:36 +0400

    Lesson-10: complited 4 seps

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
+const bookParent = document.querySelector('.books'),
+      bookAll = document.querySelectorAll('.book'),
+      body = document.querySelector('body'),
+      bookLinkAll = document.querySelectorAll('.book a'),
+      advertising = document.querySelector('.adv');
+      
+
+// console.log(bookLinkAll);
+
+bookParent.prepend(bookAll[1]);
+bookParent.append(bookAll[2]);
+bookAll[3].before(bookAll[4]);
+
+body.setAttribute('style','background-image: url(./image/you-dont-know-js.jpg)');
+
+bookLinkAll[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+advertising.remove();
+
+
 
