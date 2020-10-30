a020f942a20a2edd22fcf09855773ec05f67633d
Makarenko Viktor <nevermind.13@mail.ru>
2020-10-30 17:26:19 +0400

    Lesson-12: function add todo items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+let todoControl = document.querySelector('.todo-control'),
+    headerInput  = document.querySelector('.header-input'),
+    todoList  = document.querySelector('.todo-list'),
+    todoCompleted  = document.querySelector('.todo-completed'),
+// console.log(todoControl)
+    todoData = [
+      { value: 'Выгулять собаку',
+        complited: false },
+      { value: 'Закупить продукты',
+        complited: true }
+    ];
+
+
+let renderTodo = function(){
+
+
+  todoData.forEach(function(item){
+    let todoItem = document.createElement('li');
+    todoItem.classList.add('todo-item');
+    todoItem.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+                          '<div class="todo-buttons">' +
+                            '<button class="todo-remove"></button>' +
+                            '<button class="todo-complete"></button>' +
+                          '</div>';
+    todoList.append(todoItem);
+  });
+};
+
+
+
+todoControl.addEventListener('submit', function(event){
+  event.preventDefault();
+  let newTodo = {value: headerInput.value, complited: false};
+  todoData.push(newTodo);
+  renderTodo();
+});
+
+// console.log(todoData[0].value)
