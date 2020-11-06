686d740586891f58a23cbcc6baf29539c421e562
Makarenko Viktor <nevermind.13@mail.ru>
2020-11-06 15:53:27 +0400

    lesson-14_p1 step 1

--- script/script.js
+++ script/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let body = document.querySelector('body')
+
+function DomElement(){
+  this.selector = '.sran';  
+  this.height;  
+  this.width;  
+  this.bg;  
+  this.fontSize;
+}
+ 
+DomElement.prototype.createElement = function(){
+  if(this.selector[0] === '.'){
+    let block = document.createElement('div');
+    block.classList.add('block');
+    block.textContent = 'block';
+    body.prepend(block);
+  } else if(this.selector[0] === '#'){
+    let block = document.createElement('p');
+    block.id = 'best';
+    block.textContent = 'best';
+    body.prepend(block);
+  } 
+}
+
+let ob = new DomElement;
+
+ob.createElement()
+
