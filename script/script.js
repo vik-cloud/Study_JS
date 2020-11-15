602dfda6215e9e5d5438337ddad124a4b713fbdc
Makarenko Viktor <nevermind.13@mail.ru>
2020-11-15 17:23:54 +0400

    Lesson15_part2: complited (check: 0)

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!')
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!')
+  }
+}
+
+let second = new Second();
+second.hello()
